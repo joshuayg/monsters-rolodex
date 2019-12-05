7af0c2de90603837365edf0cfc04f79c6bda768a
Joshua Yang <joshua.yang@gettyimages.com>
2019-12-05 12:06:46 -0800

    something

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { SearchBox } from './components/search-box/search-box.component'
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       monsters: [],
